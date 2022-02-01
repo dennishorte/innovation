e960222560c8c18dc25230944263accd7694a92e
Dennis Horte <dennis@pinterest.com>
2022-02-01 13:28:22 -0800

    History; Heritage

---
+++ res/echo/achievements/History.test.js
@@ -0,0 +1,44 @@
+Error.stackTraceLimit = 100
+
+const t = require('../../../src/testutil.js')
+
+describe('History', () => {
+  test('four effects in one color', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'echo'] })
+    game.testSetBreakpoint('before-first-player', (game) => {
+      t.setColor(game, 'dennis', 'yellow', ['Chopsticks', 'Toothbrush', 'Deodorant'])
+      t.setSplay(game, 'dennis', 'yellow', 'up')
+      t.setHand(game, 'dennis', ['Barometer'])
+    })
+    const request1 = game.run()
+    const request2 = t.choose(game, request1, 'Meld.Barometer')
+
+    expect(t.cards(game, 'achievements')).toStrictEqual(['History'])
+  })
+
+  test('three effects in one color', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'echo'] })
+    game.testSetBreakpoint('before-first-player', (game) => {
+      t.setColor(game, 'dennis', 'yellow', ['Chopsticks', 'Toothbrush'])
+      t.setSplay(game, 'dennis', 'yellow', 'up')
+      t.setHand(game, 'dennis', ['Barometer'])
+    })
+    const request1 = game.run()
+    const request2 = t.choose(game, request1, 'Meld.Barometer')
+
+    expect(t.cards(game, 'achievements')).toStrictEqual([])
+  })
+
+  test('four effects spread across two colors', () => {
+    const game = t.fixtureFirstPlayer({ expansions: ['base', 'echo'] })
+    game.testSetBreakpoint('before-first-player', (game) => {
+      t.setColor(game, 'dennis', 'yellow', ['Chopsticks', 'Toothbrush', 'Deodorant'])
+      t.setSplay(game, 'dennis', 'yellow', 'up')
+      t.setHand(game, 'dennis', ['Toilet'])
+    })
+    const request1 = game.run()
+    const request2 = t.choose(game, request1, 'Meld.Toilet')
+
+    expect(t.cards(game, 'achievements')).toStrictEqual([])
+  })
+})
