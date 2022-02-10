7cc228a8331fd052cda7fcac4e311eddb2f2c431
Dennis Horte <dennis@pinterest.com>
2022-02-10 12:37:45 -0800

    Fix a bug in Augustus Caesar test

--- res/figs/AugustusCaesar.test.js
+++ res/figs/AugustusCaesar.test.js
@@ -57,21 +57,20 @@ describe('Augustus Caesar', () => {
     t.testZone(game, 'forecast', ['Optics', 'Tools'], { sort: true })
   })
 
-  test('karma (other player, using meld)', () => {
+  test.only('karma (other player, using meld)', () => {
     const game = t.fixtureFirstPlayer({ expansions: ['base', 'figs'] })
     game.testSetBreakpoint('before-first-player', (game) => {
       t.setColor(game, 'micah', 'red', ['Archery'])
       t.setColor(game, 'micah', 'green', ['Augustus Caesar'])
       t.setForecast(game, 'micah', ['Construction', 'Tools'])
-      t.setHand(game, 'dennis', ['Paper'])
+      t.setHand(game, 'dennis', ['The Wheel'])
     })
     const request1 = game.run()
-    const request2 = t.choose(game, request1, 'Meld.Paper')
+    const request2 = t.choose(game, request1, 'Meld.The Wheel')
 
     t.testZone(game, 'red', ['Archery', 'Construction'], { player: 'micah' })
     t.testZone(game, 'blue', ['Tools'], { player: 'micah' })
-    t.testZone(game, 'green', ['Paper', 'Augustus Caesar'], { player: 'micah' })
+    t.testZone(game, 'green', ['The Wheel', 'Augustus Caesar'], { player: 'micah' })
     t.testZone(game, 'forecast', [], { player: 'micah' })
-
   })
 })
