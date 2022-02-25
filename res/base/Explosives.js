7c52aee557512cdf75639af9c2f0c4f2c9204521
Dennis Horte <dennis@pinterest.com>
2022-02-25 09:15:24 -0800

    Explosives

--- res/base/Explosives.js
+++ res/base/Explosives.js
@@ -15,7 +15,32 @@ function Card() {
     `I demand you transfer the three highest cards from your hand to my hand! If you transferred any, and then have no cards in hand, draw a {7}.`
   ]
 
-  this.dogmaImpl = []
+  this.dogmaImpl = [
+    (game, player, { leader }) => {
+      const target = game.getZoneByPlayer(leader, 'hand')
+
+      let cards = game.getCardsByZone(player, 'hand')
+      let numTransferred = 0
+
+      while (numTransferred < 3 && cards.length > 0) {
+        const highest = game.utilHighestCards(cards)
+        cards = cards.filter(card => !highest.includes(card))
+
+        if (numTransferred + highest.length <= 3) {
+          game.aTransferMany(player, highest, target)
+          numTransferred += highest.length
+        }
+        else {
+          game.aChooseAndTransfer(player, highest, target, { count: 3 - numTransferred })
+          break
+        }
+      }
+
+      if (numTransferred && game.getCardsByZone(player, 'hand').length === 0) {
+        game.aDraw(player, { age: game.getEffectAge(this, 7) })
+      }
+    }
+  ]
   this.echoImpl = []
   this.inspireImpl = []
   this.karmaImpl = []
